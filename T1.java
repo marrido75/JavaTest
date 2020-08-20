db02b380c1cd111a83432b706396a0e4c608c128
marrido <marridohssm@hotmail.com>
2020-08-20 23:41:46 +0800

    1311

--- T1.java
+++ T1.java
@@ -2,5 +2,6 @@ public class T1{
     public static void main(String[] args) {
         System.out.print("obj");
         System.out.println("我才是一饿宝贝");
+        System.out.println("我4认为4日贝");
     }
 }
