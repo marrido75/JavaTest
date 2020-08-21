a218b2a69e901284020d6315e678f004020736cf
marrido <marridohssm@hotmail.com>
2020-08-21 23:42:26 +0800

    23.42

--- T1.java
+++ T1.java
@@ -1,14 +1,17 @@
-public class T1{
+public class T1 {
     public static void main(String[] args) {
-        intSun(900, 15646);
+        T1.intSun(317, 180);
+        double a = T1.intSun2(100, 200);
+        System.out.println(a);
     }
 
-    public static void intSun(int x , int y) {
-        int z = x+y;
+    public static void intSun(int x, int y) {
+        int z = x - y;
         System.out.println(z);
     }
 
-
-
+    public static int intSun2(int x, int y) {
+        return x + y;
+    }
 
 }
